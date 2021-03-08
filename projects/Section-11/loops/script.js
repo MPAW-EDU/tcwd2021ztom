2fa8a2ddbb9f6724b364bed18d8d46fe0716c6ad
Michael Walter <michael_patrick.walter@outlook.com>
2021-03-07 23:24:54 -0800

    Section 11: demonstrating different types of for loop syntax.

--- projects/Section-11/loops/script.js
+++ projects/Section-11/loops/script.js
@@ -6,3 +6,17 @@ let todo = [
     "Push Code",
     "Study Math"
 ];
+
+for (let i = 0; i < todo.length; i++){
+    console.log(todo[i]);
+    todo[i] = `${todo[i]}!`
+    console.log(todo[i]);
+
+}
+
+for (item of todo) {
+    console.log(item);
+    item = `${item}!`
+    console.log(item);
+
+}
