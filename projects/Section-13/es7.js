2e82348b76f8073b3bd30ebfcb5e7e293cdbb8a9
Michael Walter <michael_patrick.walter@outlook.com>
2021-03-10 23:43:32 -0800

    Section 13: Reviewed ES7 syntax changes from 2016.

---
+++ projects/Section-13/es7.js
@@ -0,0 +1,10 @@
+
+// ES7 Syntax, 2016 had 2 main additions
+
+// .includes() was added to check if something was included in a string or object/array
+const aString ="stringy"
+console.log(aString.includes('y'))
+
+// **  was the second change in 2016, the use is to be able to multiple by exponents.
+const square = (n) => n**2;
+console.log(square(2));
