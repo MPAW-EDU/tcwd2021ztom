336b1e220fc163609bb1904525aa3116b9941bfc
Michael Walter <michael_patrick.walter@outlook.com>
2021-03-11 20:41:46 -0800

    Make a swap function for later user in a project.

--- JSTests.js
+++ JSTests.js
@@ -1,23 +1,36 @@
 
 
-console.log(5-"4");
+// console.log(5-"4");
 
-console.log(5 % 10);
+// console.log(5 % 10);
 
-console.log(true + true + true);
+// console.log(true + true + true);
 
-console.log();
+// console.log();
 
-console.log("a" > 'A');
+// console.log("a" > 'A');
 
-console.log("b" < "A");
+// console.log("b" < "A");
 
-const borderline = [true, true, true]
+// const borderline = [true, true, true]
 
-const redline = borderline.reduce((a,b) => a + b)
+// const redline = borderline.reduce((a,b) => a + b)
 
-if (redline === 3) {
-    console.log("Perimeter Breached!");
-} else {
-    console.log("Perimeter Secure");
+// if (redline === 3) {
+//     console.log("Perimeter Breached!");
+// } else {
+//     console.log("Perimeter Secure");
+// }
+
+let a = 3;
+let b = 1;
+let array = [1,3,4,2,6]
+
+const swap = (array, a, b) => {
+    const temp = array[a]
+    array[a] = array[b];
+    array[b] = temp;
+    return array
 }
+
+console.log(swap(array,a,b));
