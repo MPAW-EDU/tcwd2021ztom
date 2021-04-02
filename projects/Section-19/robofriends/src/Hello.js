5ce6131d2b75d4c7de99ca0d1bdb20117e2884c5
Michael Walter <michael_patrick.walter@outlook.com>
2021-04-01 22:02:43 -0700

    Section 19: tachyons is interesting, it's very similar to bootstrap but much more concise.

--- projects/Section-19/robofriends/src/Hello.js
+++ projects/Section-19/robofriends/src/Hello.js
@@ -1,12 +1,14 @@
 
 import React, { Component } from 'react'
 
+import './hello.css';
 
 export default class Hello extends Component {
     render() {
         return (
-            <div>
+            <div className="f1 tc">
                 <h1>Hello Component</h1>
+                <p>Welcome to React</p>
             </div>
         )
     }
