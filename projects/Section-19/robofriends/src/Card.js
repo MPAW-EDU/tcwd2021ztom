eac0b4ece496028e51d010b87f61768865e33b42
Michael Walter <michael_patrick.walter@outlook.com>
2021-04-02 12:23:40 -0700

    Section 19: Created CardList and Card components, passed robots data into CardList as props, deconstructed it to robots. Then moved the mapping into CardList and imported Card to CardList. This exercise helped me to correct a misconception I had about file imports.

--- projects/Section-19/robofriends/src/Card.js
+++ projects/Section-19/robofriends/src/Card.js
@@ -1,17 +1,17 @@
 
 import React from 'react'
 
-const Card = ({name, username, email}) => {
+const Card = ({name, username, email, id}) => {
     return (
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img src={`https://robohash.org/${username}?200x200`} alt="robots"/>
+            <img src={`https://robohash.org/${id}?200x200`} alt="robots"/>
+            <h2>"{username}"</h2>
             <h2>{name}</h2>
             <p>{email}</p>
         </div>
     )
 }
 
-export default Card
-
+export default Card;
 
 
