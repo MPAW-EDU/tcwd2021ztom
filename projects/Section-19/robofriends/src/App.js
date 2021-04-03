461f63b3f0acdb971f7f6cccd8dcc78b519c25ef
Michael Walter <michael_patrick.walter@outlook.com>
2021-04-02 23:21:19 -0700

    Section 19: Search functionality is up and working.

--- projects/Section-19/robofriends/src/App.js
+++ projects/Section-19/robofriends/src/App.js
@@ -1,15 +1,36 @@
 
-import React from 'react'
+import React, { Component, useState } from 'react'
 import CardList from './CardList';
+import Searchbox from './Searchbox';
 
 import { robots } from './robots';
 
-const App = () => {
-    return (
-        <div>
-            <CardList robots={robots} />
-        </div>
-    )
-}
+export default class App extends Component {
 
-export default App
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+    render(){
+
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <Searchbox onSearchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots} />
+            </div>
+        )
+    }
+
+}
