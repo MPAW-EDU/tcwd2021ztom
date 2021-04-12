0a51f9d823f3f3f26c841351ef08c3acbd084caf
Michael Walter <michael_patrick.walter@outlook.com>
2021-04-12 14:22:39 -0700

    Section 23: built out basic file structure and navigation component.

---
+++ projects/Section-23/facerecognitionbrain/src/components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <div>
+            <nav>
+                <p>Sign Out</p>
+            </nav>
+        </div>
+    )
+}
+
+export default Navigation
