9c7bb5951c1db0461be332622b8afb6fd12f6fc2
Michael Walter <michael_patrick.walter@outlook.com>
2021-04-12 16:39:54 -0700

    Section 23: Built out imagelinkform, rank, logo, navigation components. Added a new font and set styles using css and tachyons.

--- projects/Section-23/facerecognitionbrain/src/App.js
+++ projects/Section-23/facerecognitionbrain/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/*<ImageLinkForm />
-      <FaceRecognition /> */}
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition /> */}
     </div>
   );
 }
