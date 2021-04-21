e5fba3265146a3d28bc2e750643287cd461a3750
Michael Walter <michael_patrick.walter@outlook.com>
2021-04-20 21:56:43 -0700

    Section 23: facerecognition, started building out register component and matching route.

--- projects/Section-23/facerecognitionbrain/src/components/Register/Register.js
+++ projects/Section-23/facerecognitionbrain/src/components/Register/Register.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Signin = ({ onRouteChange }) => {
+const Register = () => {
     return (
         <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
             <main className="pa4 black-80">
@@ -8,8 +8,8 @@ const Signin = ({ onRouteChange }) => {
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                     <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                     <div className="mt3">
-                        <label className="db fw6 lh-copy f6" for="email-address">Email</label>
-                        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                        <label className="db fw6 lh-copy f6" for="name">Name</label>
+                        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
                     </div>
                     <div className="mv3">
                         <label className="db fw6 lh-copy f6" for="password">Password</label>
@@ -23,13 +23,10 @@ const Signin = ({ onRouteChange }) => {
                         type="submit" 
                         value="Sign in" />
                     </div>
-                    <div className="lh-copy mt3">
-                    <p onClick={() => onRouteChange('register')} className="f6 link dim black db">Register</p>
-                    </div>
                 </form>
             </main>
         </article>
     )
 }
 
-export default Signin
+export default Register
