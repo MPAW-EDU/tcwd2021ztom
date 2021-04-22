d7f8186f4d255e81d53805c35ee16b7ad985c86e
Michael Walter <michael_patrick.walter@outlook.com>
2021-04-21 22:44:02 -0700

    Section 24: built our a few generic get endpoints and one post endpoint.

--- projects/Section-24/server.js
+++ projects/Section-24/server.js
@@ -4,17 +4,43 @@ const express = require('express');
 let PORT = process.env.PORT;
 if (PORT === null || PORT === undefined || PORT === '') PORT = 5050
 
-const server = express();
-
-const user = {
-    name: 'Bobby',
-    sanity: 'CraiCrai'
-}
-
-server.get('/', (req,res) => {
-    res.status(200).send(user)
+const app = express();
+app.use((req, res, next) => {
+    console.log('<h1>Hello</h1>')
+    next();
 })
 
-server.listen(PORT, () => {
+// Replacement for body-parser
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+let users = [
+    {    name: 'Bobby',
+        sanity: 'CraiCrai'
+    },
+]
+
+app.get('/', (req,res) => {
+    res.status(200).send('<h1>Testing!!</h1>')
+})
+
+app.get('/profile', (req,res) => {
+    res.status(200).send(user);
+})
+
+app.post('/profile', (req,res) => {
+    const {name, sanity} = req.body;
+
+    const newUser = {
+        name,
+        sanity
+    }
+
+    users.push(newUser)
+
+    res.status(200).send(users);
+})
+
+app.listen(PORT, () => {
     console.log(`Server Online: http://localhost:${PORT}/`);
 })
