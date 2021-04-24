84d6a6662e28b9208e57adeb7865e2a0a55fac24
Michael Walter <michael_patrick.walter@outlook.com>
2021-04-23 21:52:15 -0700

    Section 25: smartbrain-api, finished building out basic api setup, will refactor and improve tomorrow, then modify logic to incorporate a database.

--- projects/Section-25/smartbrain-api/server.js
+++ projects/Section-25/smartbrain-api/server.js
@@ -73,7 +73,7 @@ app.post('/register', (req,res) => {
 
     database.users.push(newUser);
 
-    res.status(201).json(database.users)
+    res.status(201).json(database.users[database.users.length-1]);
 
 })
 
@@ -81,14 +81,45 @@ app.post('/register', (req,res) => {
  *  /profile/:userId, get
  *  returns user info
  */
-app.get(`/profile/:`, (req,res) => {
-    res.send('User Id')
+app.get(`/profile/:id`, (req,res) => {
+   const { id } = req.params;
+   let found = false;
+   
+   database.users.forEach(user => {
+       if (user.id === Number(id)) {
+           found = true;
+           return res.status(200).json(user)
+       }
+   })
+
+   if (!found) {
+       res.status(400).json('No such user.')
+   }
+
 })
 
 /**
  *  /image, put
  *  returns updated user object or data
  */
+app.put('/image', (req,res) => {
+
+    const { id } = req.body;
+    let found = false;
+
+    database.users.forEach(user => {
+        if ( user.id === Number(id) ){
+            found = true;
+            ++user.entries;
+            return res.status(201).json(`Entries Increased ${user.name}: ${user.entries}`)
+        }
+    })
+
+    if (!found) {
+        res.status(400).json('No such user exists.')
+    }
+
+})
 
 
 
