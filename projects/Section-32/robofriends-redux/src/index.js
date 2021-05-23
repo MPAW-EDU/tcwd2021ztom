35615cb5ec0b9d449cf5a35c6e0c0e753c6087ea
Michael Walter <michael_patrick.walter@outlook.com>
2021-05-23 12:39:08 -0700

    Section-32: Setup basic redux to manage state, working now on connect.

--- projects/Section-32/robofriends-redux/src/index.js
+++ projects/Section-32/robofriends-redux/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import { createStore } from 'redux';
+import { Provider, Connect } from 'react-redux';
 import './index.css';
 import 'tachyons';
+import { searchRobots } from './redux/reducer';
 
+
+const store = createStore(searchRobots);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
